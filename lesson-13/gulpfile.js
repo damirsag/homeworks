913852df3b68f94f63e2e78b416a39fcfda28d53
Damir Sagiev <damir.s1@mail.ru>
2019-12-02 08:19:20 +0300

    update gulp minify css

--- lesson-13/gulpfile.js
+++ lesson-13/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 
 gulp.task('hello', function(done) {
@@ -20,7 +21,14 @@ gulp.task('browser-sync', function() {
 
 //clean CSS task
 gulp.task('minify-css', () => {
+
   return gulp.src('./css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('css'));
+
+
 });
+
