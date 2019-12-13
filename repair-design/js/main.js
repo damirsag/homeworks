4b0a5eb5f6a26b60e7b2e7f7a8165fcd0b972fa2
Damir Sagiev <damir.s1@mail.ru>
2019-12-13 14:43:12 +0300

    feat: add button:return

--- repair-design/js/main.js
+++ repair-design/js/main.js
@@ -1,16 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	const modal = document.querySelector('.modal')
-	const modalBtn = document.querySelectorAll('[data-toggle=modal]')
-	const closeBtn = document.querySelector('.modal__close')
-	const switchModal = () => {
-		modal.classList.toggle('modal_visible');
-	}
+$(document).ready(function () {
+	var modal = $('.modal'),
+			modalBtn = $('[data-toggle=modal]'),
+			closeBtn = $('.modal__close'),
+			retBtn = $('.returnButton');
 
-
-	modalBtn.forEach(element => {
-		element.addEventListener('click', switchModal);
+	modalBtn.on('click', function () {
+		modal.toggleClass('modal_visible');	
 	});
-
-	closeBtn.addEventListener('click', switchModal);
+	closeBtn.on('click', function() {
+		modal.toggleClass('modal_visible');
+	});
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 500) {
+			retBtn.addClass('btnShow');
+		} else {
+			retBtn.removeClass('btnShow');
+		}
+	});
+	retBtn.on('click', function() {
+		$('html, body').animate({scrollTop:0}, '300');
+	});
 
 });
