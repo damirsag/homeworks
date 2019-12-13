20b603e9c574dc368307c132d033f4f2a120f824
Damir Sagiev <damir.s1@mail.ru>
2019-12-13 15:56:01 +0300

    refract: section design title

--- repair-design/js/main.js
+++ repair-design/js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 		}
 	});
 	retBtn.on('click', function() {
-		$('html, body').animate({scrollTop:0}, '300');
+		$('html, body').animate({scrollTop:0}, '6');
 	});
 
 });
