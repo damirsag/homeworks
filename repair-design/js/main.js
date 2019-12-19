957be299fb8f2783dcb4aac1fbb82f699d1d9fe2
Damir Sagiev <damir.s1@mail.ru>
2019-12-19 09:25:15 +0300

    feat: add section projects with slider

--- repair-design/js/main.js
+++ repair-design/js/main.js
@@ -21,4 +21,23 @@ $(document).ready(function () {
 		$('html, body').animate({scrollTop:0}, '300');
 	});
 
+	var mySwiper = new Swiper ('.swiper-container', {
+		// Optional parameters
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
+
+	var nextBtn = $('.swiper-button-next');
+	var prevBtn = $('.swiper-button-prev');
+	var bullets = $('.swiper-pagination');
+
+	nextBtn.css('left', prevBtn.width() + 10 + bullets.width() + 10)
+	bullets.css('left', + prevBtn.width() + 10)
 });
